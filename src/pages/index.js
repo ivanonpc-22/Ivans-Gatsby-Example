0952ddb22829efc58ba4f522ad6f84a4b49a4de7
Ivan <owner@ivanservers.com>
2022-02-03 11:35:12 +0900

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,8 +8,8 @@ import Seo from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <h1>Ivan Ryan</h1>
+    <p>A Gatsby Frontend Site</p>
     <p>Now go build something great.</p>
     <StaticImage
       src="../images/gatsby-astronaut.png"
@@ -20,10 +20,7 @@ const IndexPage = () => (
       style={{ marginBottom: `1.45rem` }}
     />
     <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
-      <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
-      <Link to="/using-dsg">Go to "Using DSG"</Link>
+
     </p>
   </Layout>
 )
